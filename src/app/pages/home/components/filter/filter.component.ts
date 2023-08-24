eeeff6638d969758e2144a8c70ffaa7008dfc315
amiraallagui <allagui_amira@outlook.fr>
2023-08-24 10:22:24 +0100

    filter

---
+++ src/app/pages/home/components/filter/filter.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-filter',
+  templateUrl: `filter.component.html`,
+  styles: [
+  ]
+})
+export class FilterComponent {
+  categories=['shoes', 'sports'];
+
+}
--- src/app/pages/home/components/products-header/products-header.component.ts
+++ src/app/pages/home/components/products-header/products-header.component.ts
@@ -1,9 +1,24 @@
-import { Component } from '@angular/core';
+import { Component,EventEmitter ,Output} from '@angular/core';
 
 @Component({
   selector: 'app-products-header',
   templateUrl: `products-header.component.html`,
 })
 export class ProductsHeaderComponent {
+ @Output() columnsCountChange = new EventEmitter<number>();
+  sort = 'sort';
+  itemsShowCount = 12;
 
+  constructor() {}
+  ngOnInit(): void {}
+
+  onSortUpdated(newSort: string):void {
+    this.sort = newSort;
+  }
+  onItemsUpdated(count: number):void {
+    this.itemsShowCount = count;
+  }
+  onColumnsUpdated(colsNum: number):void {
+    this.columnsCountChange.emit(colsNum);
+  }
 }
