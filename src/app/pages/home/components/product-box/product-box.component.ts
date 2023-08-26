d5a15404688a730050f7350ace73ab5839730fde
amiraallagui <allagui_amira@outlook.fr>
2023-08-27 00:41:48 +0100

    product-box

--- src/app/pages/home/components/product-box/product-box.component.ts
+++ src/app/pages/home/components/product-box/product-box.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { Component,Input} from '@angular/core';
 
 @Component({
   selector: 'app-product-box',
-  templateUrl: `product-box.component.html`,
+  templateUrl: `./product-box.component.html`,
 })
 export class ProductBoxComponent {
+  @Input() fullWidthMode = false;
+
 
 }
