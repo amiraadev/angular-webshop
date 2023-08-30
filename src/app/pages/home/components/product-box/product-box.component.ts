7a9d29299983ab261f3d3c96225d2370b34aad63
amiraallagui <allagui_amira@outlook.fr>
2023-08-30 17:17:57 +0100

    30-08-2023

--- src/app/pages/home/components/product-box/product-box.component.ts
+++ src/app/pages/home/components/product-box/product-box.component.ts
@@ -7,14 +7,7 @@ import { Product } from 'src/app/models/product.model';
 })
 export class ProductBoxComponent {
   @Input() fullWidthMode = false;
-  product :Product | undefined ={
-    id : 1,
-    title : "Snickers",
-    price : 150,
-    category : 'shoes',
-    description : 'Description',
-    image : "https://via.placeholder.com/150"
-  };
+  @Input() product :Product | undefined;
 
   @Output() addToCart = new EventEmitter();
 
